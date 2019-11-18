406dfed818cda1194c572ddce593d87ea2229f57
Barzov-Pavel <barzov_pavel@mail.ru>
2019-11-18 12:52:24 +0300

    Change import in runner

--- src/Runner/Runner.java
+++ src/Runner/Runner.java
@@ -2,9 +2,7 @@ package Runner;
 
 import Comparators.MedicineComparator;
 import domain.Medicine;
-import parcers.MedicinesXMLReader;
-import parcers.MedicinesXMLValidator;
-import parcers.MedicinesXMLWriter;
+import parcers.*;
 
 import javax.xml.stream.XMLStreamException;
 import java.io.FileNotFoundException;
